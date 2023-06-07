4372b758342c6119937e49daf454cd0e68ac531d
Cheater121 <106826455+Cheater121@users.noreply.github.com>
2023-06-07 09:21:23 +0300

    Initial

---
+++ secret_string_changer_by_example.py
@@ -0,0 +1,15 @@
+import string, random
+
+secret_string = "INSERT YOUR SECRET STRING"
+
+example_output = ""
+for i in secret_string:
+    if i.isdigit():
+        i = random.randint(0, 9)
+        example_output += str(i)
+    elif i.isalpha():
+        i = random.choice(string.ascii_letters)
+        example_output += i
+    else:
+        example_output += i
+print(example_output)
